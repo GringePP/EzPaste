04e7815b700ae86f45b8a50afc54088e04e54d5d
Mindjet <pearl920@outlook.com>
2019-11-25 23:44:11 +0800

    [+]: use bat to copy image from system clipboard

--- extension.js
+++ extension.js
@@ -1,6 +1,6 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
+const vscode = require("vscode");
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -9,22 +9,22 @@ const vscode = require('vscode');
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log('Congratulations, your extension "ezpaste" is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "ezpaste" is now active!');
+  const disposable = vscode.commands.registerCommand(
+    "extension.ezpaste",
+    function() {
+      const platform = process.platform;
+      if (platform == "win32") {
+        const execWin32 = require("child_process").spawn;
+        execWin32(__dirname + "/scripts/pasteFromClip.bat");
+      }
+    }
+  );
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!sss');
-	});
-
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 exports.activate = activate;
 
@@ -32,6 +32,6 @@ exports.activate = activate;
 function deactivate() {}
 
 module.exports = {
-	activate,
-	deactivate
-}
+  activate,
+  deactivate
+};
---
+++ scripts/pasteFromClip.bat
@@ -0,0 +1,3 @@
+@echo off
+cd /d %~dp0
+nircmd.exe clipboard saveimage "ezpaste-temp.png" 
